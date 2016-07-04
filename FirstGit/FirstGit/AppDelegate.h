0ba1b458d8ea2d747c800b74e05f39272e08d181
zhangji <635888649@qq.com>
2016-07-04 16:37:55 +0800

    First commit

---
+++ FirstGit/FirstGit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FirstGit
+//
+//  Created by zhangji on 16/7/4.
+//  Copyright © 2016年 zhangji. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FirstGit/FirstGit/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  FirstGit
+//
+//  Created by zhangji on 16/7/4.
+//  Copyright © 2016年 zhangji. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
